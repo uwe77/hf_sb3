12f72e8ce833d4a791a4fefa7b7c4aae062030c7
uwe <uwe90711@gmail.com>
2024-02-18 00:51:43 +0800

    modify sorter

--- web_crawler/web_sorter.py
+++ web_crawler/web_sorter.py
@@ -2,19 +2,35 @@ import json, sys, re
 import pandas as pd
 
 
-def sort_by_world(data, world):
-    result = [i for i in data if world in i['title']]
+def sum_of_world(world, key='describe'):
+    for i in range(13):
+        filename = f'hf_rl_fullsearch_p{i+1}.json'
+        result = 0
+        with open(filename, 'r') as f:
+            data = json.load(f)
+            sys.stdout.write("\r")
+            sys.stdout.write("**************Processing page {:2d}**************".format(i+1))
+            sys.stdout.flush()
+            for d in data: 
+                if world in d[key]:
+                    result += 1
+    sys.stdout.write("\rComplete!                                      \n")
     return result
 
-def sort_by_author(data):
-    authors = list(set([i['author'] for i in data]))
-    result = {i: [] for i in authors}
-    for i in data:
-        result[i['author']].append(i)
+def sun_of_author(author, key='author'):
+    for i in range(13):
+        filename = f'hf_rl_fullsearch_p{i+1}.json'
+        result = 0
+        with open(filename, 'r') as f:
+            data = json.load(f)
+            sys.stdout.write("\r")
+            sys.stdout.write("**************Processing page {:2d}**************".format(i+1))
+            sys.stdout.flush()
+            for d in data: 
+                if author in d[key]:
+                    result += 1
+    sys.stdout.write("\rComplete!                                      \n")
     return result
 
 if __name__ == "__main__":
-    for i in range(1, 13):
-        filename = f'hf_rl_fullsearch_p{i}.json'
-        with open(filename, 'r') as f:
-            data = json.load(f)
+    print(sum_of_world('a2c'))
